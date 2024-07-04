d5c071d51e46f7f251f59e8dc03e783eb5c55c5d
Tyrone Mckenzie Jr <tyronemckenzie30@gmail.com>
2024-07-04 10:01:16 -0600

    change

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 name_1 = input("What is your name: ")
-print("Hello", name_1)
+age_1 = int(input("What is your age: "))
+
+info = [name_1, age_1]
+print("Hello", info[0])
+if info[1] >= 18:
+    print("Your an adult. ")
+elif info[1] < 18 and info[1] >= 13:
+    print("Your a teenager. ")
+elif info[1] < 13:
+    print("Your a child")
